ba9226d62350ef78299ae82062306ca9f87e5945
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-08-16 22:22:41 +0300

    add simple component

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
-console.log('hello world')
+import React from 'react'
+import { render } from 'react-dom'
+
+import Article from './components/Article'
+
+render(<Article />, document.getElementById('container'))
---
+++ src/components/Article.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Article() {
+    return (
+        <div>
+            <h3>Some title</h3>
+            <section>
+                Article body
+            </section>
+        </div>
+    )
+}
